83201f4295857437658dc7aa4a1e823f8e2b8891
Philip Horger <campadrenalin@gmail.com>
2012-04-13 18:56:55 -0700

    Moved in old util/server from ConcurrenTree

---
+++ ctserver/server.py
@@ -0,0 +1,17 @@
+class Server(object):
+	''' Base class for ServerPool Servers. '''
+	def run(self):
+		''' Start the server running '''
+		raise NotImplementedError()
+
+	def close(self):
+		''' Stop the server, terminating self.run '''
+		raise NotImplementedError()
+
+	@property
+	def properties(self):
+		result = {}
+		for name in dir(self):
+			if name != "properties":
+				result[name] = getattr(self, name)
+		return result
